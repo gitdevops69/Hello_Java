07fd338b6c871bd4d8962e0f6c610015a695abad
gitdevops69 <105194929+gitdevops69@users.noreply.github.com>
2022-05-23 10:04:30 +0530

    hello
    hello

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, ");
+      System.out.println("Hello, varma ");
    }
 }
